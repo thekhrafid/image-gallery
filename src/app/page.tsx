04efc249891174b294575eec33e9cc87ae93f216
thekhrafid <mdrafidhossen44@gmail.com>
2025-10-04 15:57:55 +0600

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home(){
+  return(
+    <main className="flex justify-center items-center h-screen">
+      <h1 className="text-green-500 text-5xl font-bold">this is my first project</h1>
+    </main>
+  )
+}
